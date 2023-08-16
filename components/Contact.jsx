84dae0dbc08ff0e291027885792c4c84ca589571
SahilChowkekar <chowkekar95@gmail.com>
2023-08-16 07:15:43 -0700

    adding updated contact.jsx and main.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
                   </div>
                   </a>
                  
-                  <a href='https://drive.google.com/file/d/1tqyAUJfFcrJQHdC1VlbxsAtlJa87WTE3/view?usp=sharing'
+                  <a href='https://drive.google.com/file/d/1AAhuJM0d9g6RzaYtXCbU-P6yt59fdTtR/view?usp=sharing'
                       target='_blank'
                       rel='noreferrer'>
                       <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
